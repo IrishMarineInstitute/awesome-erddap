46393bcc0af26526fc81896b8ac70854f9628a87
Rob Fuller <fullergalway@gmail.com>
2020-03-27 08:44:55 +0000

    added jsonp file

--- tools/build.js
+++ tools/build.js
@@ -2,12 +2,17 @@
 	const fs = require('fs');
 	const jsonData = require('../erddaps.json');
 	const outData = 'var awesomeErddaps = ' + JSON.stringify(jsonData) + ';';
-	fs.writeFile("erddaps.js", outData, function(err) {
+	const jsonpData = 'awesomeErddapsCb(' + JSON.stringify(jsonData) + ');';
+	const writeFile = (filename,data) => {
+          fs.writeFile(filename, data, function(err) {
 		if(err) {
 			return console.log(err);
 		}
 		else {
-			console.log('[awesome-erddap] erddaps.js re-written...');
+			console.log(`[awesome-erddap] ${filename} re-written...`);
 		}
-	}); 
+	  }); 
+       };
+       writeFile("erddaps.js",outData);
+       writeFile("erddaps.jsonp",jsonpData);
 })();
