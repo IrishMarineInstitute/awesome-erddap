08371c5ad24b93c0ad14f2b1af6ad7d464dac5bc
DsmythMI <damian.smyth@marine.ie>
2022-07-01 19:30:20 +0000

    changed ajv json schema draft from 04 to 07

--- test/JsonMatchesJsonSchema.js
+++ test/JsonMatchesJsonSchema.js
@@ -2,7 +2,7 @@
 	var Ajv = require('ajv');
 	var assert = require('assert');
 	var ajv = new Ajv({schemaId: 'id'});
-	ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
+	ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-07.json'));
 	const jsonData = require('../erddaps.json');
 	const jsonSchema = require('../json-schema/ErddapInstancesSchema.json');
 	assert(ajv.addSchema(jsonSchema,'erddapSchema').validate('erddapSchema',jsonData));
